fa5bfa4bc24aa9e35063c8b29cc3b5a0b41cb210
d-alshehri <10674@holbertonstudents.com>
2025-07-28 16:21:07 +0000

    Pycodestyle validation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,6 +4,7 @@ Module 0-add
 This module provides a function to add two float numbers.
 """
 
+
 def add(a: float, b: float) -> float:
     """
     Adds two float numbers and returns their sum as a float.
