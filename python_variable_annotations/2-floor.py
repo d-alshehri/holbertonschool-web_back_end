1714ae461a940210a3ae967c081313ecffd9b932
d-alshehri <10674@holbertonstudents.com>
2025-07-28 16:29:04 +0000

    Add type-annotated function floor using math.floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module 2-floor
+Provides a function that returns the floor of a float.
+"""
+
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float number.
+
+    Args:
+        n (float): The number to apply floor to.
+
+    Returns:
+        int: The floor of the input float.
+    """
+    return math.floor(n)
