0c8570cb7c2bb3354b2c5b2a7aca82c950dd33ed
d-alshehri <10674@holbertonstudents.com>
2025-07-29 15:16:12 +0000

    Create task_wait_random function that returns a scheduled asyncio.

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Creates and returns an asyncio.Task that runs wait_random."""
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Returns an asyncio.Task for wait_random with given max_delay."""
+    return asyncio.create_task(wait_random(max_delay))
