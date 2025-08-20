0dbb33ca563d5686dad7497bd9f5a24dd4d8bbdb
d-alshehri <10674@holbertonstudents.com>
2025-08-20 10:50:29 +0000

    Fixed code styling

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -3,6 +3,6 @@ export default function appendToEachArrayValue(array, appendString) {
     for (const idx of array) {
       arrayEnd.push(`${appendString}${idx}`);
     }
-  
+
     return arrayEnd;
   }
