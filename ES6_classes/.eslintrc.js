4dc7b875d155d94b23c2f87a362454611de5608a
d-alshehri <10674@holbertonstudents.com>
2025-08-21 10:30:27 +0000

    Updates

--- ES6_classes/.eslintrc.js
+++ ES6_classes/.eslintrc.js
@@ -1,10 +1,37 @@
 module.exports = {
   env: {
-    browser: true,
+    browser: false,
     es6: true,
+    jest: true,
   },
-  extends: ['airbnb-base'],
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
   rules: {
-    'no-underscore-dangle': ['error', { allow: ['_maxStudentsSize'] }],
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
