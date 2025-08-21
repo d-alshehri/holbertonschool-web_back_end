90f3524942ff54f17be99b2aee095bfc670f1239
d-alshehri <10674@holbertonstudents.com>
2025-08-21 10:21:52 +0000

    Updated .eslintrc.js to turn off jest/no-large-snapshots rule

--- ES6_classes/.eslintrc.js
+++ ES6_classes/.eslintrc.js
@@ -1,12 +1,20 @@
 module.exports = {
   env: {
-    browser: true,
+    browser: false,
     es6: true,
+    jest: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base", "plugin:jest/all"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
   rules: {
-      'no-underscore-dangle': ['error', { allow: ['_maxStudentsSize'] }],
+    "no-underscore-dangle": ["error", { allow: ["_maxStudentsSize"] }],
+    "jest/no-large-snapshots": "off",
   },
 };
