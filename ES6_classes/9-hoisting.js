0eb1807a41f7e2588c793917b700132392b67c32
d-alshehri <10674@holbertonstudents.com>
2025-08-21 11:23:59 +0000

    Update

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,5 +1,3 @@
-// 9-hoisting.js
-
 export class HolbertonClass {
     constructor(year, location) {
       this._year = year;
@@ -31,7 +29,8 @@ export class HolbertonClass {
     }
   
     get fullStudentDescription() {
-      return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+      return `${this._firstName} ${this._lastName} - \
+  ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
     }
   }
   
@@ -44,5 +43,6 @@ export class HolbertonClass {
   const student4 = new StudentHolberton('Donald', 'Bush', class2019);
   const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
   
-  export const listOfStudents = [student1, student2, student3, student4, student5];
+  const listOfStudents = [student1, student2, student3, student4, student5];
   
+  export default listOfStudents;
