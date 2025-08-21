ea281473e6e017f7f510a64d0a523b696c783b65
d-alshehri <10674@holbertonstudents.com>
2025-08-21 10:40:35 +0000

    Update .eslintrc.js

--- ES6_classes/.eslintrc.js
+++ ES6_classes/.eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   plugins: ['jest'],
   rules: {
+    'jest/no-large-snapshots': 'off',
     'max-classes-per-file': 'off',
     'no-underscore-dangle': 'off',
     'no-console': 'off',
