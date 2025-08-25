ab30b3e90ae3c1eaaeedd480e93ddd80d78e0eb3
d-alshehri <10674@holbertonstudents.com>
2025-08-25 13:42:35 +0000

    fix(cleanSet): handle empty or undefined startString

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,6 @@
 export default function cleanSet(set, startString) {
+    if (!startString) return ''; // handle missing startString
+  
     return Array.from(set)
       .filter(value => value.startsWith(startString))
       .map(value => value.slice(startString.length))
