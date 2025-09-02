4cc8f131056e84b5d6be2b1864c586a582f324f8
d-alshehri <10674@holbertonstudents.com>
2025-09-02 13:57:23 +0000

    feat: create 1-stdin.js to read and display user input

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,7 @@
+console.log("Welcome to Holberton School, what is your name?\n");
+process.stdin.on("data", (input)=> {
+    const name = input.toString().trim();
+    console.log("Your name is: " + name);
+    process.exit();
+})
+process.on('exit', () => {console.log("This important software is now closing")});
