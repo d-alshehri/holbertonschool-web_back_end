a8085043ec37c80b0629d32437bf6007a63ea9f5
d-alshehri <10674@holbertonstudents.com>
2025-09-03 13:21:07 +0000

    fix: rewrite 1-stdin.js to remove extra newline and export function

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,16 @@
-console.log("Welcome to Holberton School, what is your name?\n");
-process.stdin.on("data", (input)=> {
-    const name = input.toString().trim();
-    console.log("Your name is: " + name);
-    process.exit();
-})
-process.on('exit', () => {console.log("This important software is now closing")});
+function startProgram() {
+    console.log("Welcome to Holberton School, what is your name?");
+  
+    process.stdin.on("data", (input) => {
+      const name = input.toString().trim();
+      console.log("Your name is: " + name);
+      process.exit();
+    });
+  
+    process.on("exit", () => {
+      console.log("This important software is now closing");
+    });
+  }
+  
+  module.exports = startProgram;
+  
